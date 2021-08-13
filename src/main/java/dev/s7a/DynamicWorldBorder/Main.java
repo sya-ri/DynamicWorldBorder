2aab7e358dddaabdea94c928b4ac51788e556a8e
sya-ri <34268371+sya-ri@users.noreply.github.com>
2021-08-13 13:43:01 +0900

    Initial commit

---
+++ src/main/java/dev/s7a/DynamicWorldBorder/Main.java
@@ -0,0 +1,10 @@
+package dev.s7a.DynamicWorldBorder;
+
+import org.bukkit.plugin.java.*;
+
+@SuppressWarnings("unused")
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
